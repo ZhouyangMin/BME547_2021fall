38c8e932a0a17a4b0751f19b3d7267464fd0c2f2
Zhouyang Min <zm66@duke.edu>
2021-08-30 12:52:01 -0400

    add blood_calculation

---
+++ blood_calculation.py
@@ -0,0 +1,8 @@
+def interface():
+    print('Blood Calculators')
+    print('Make a choice')
+    print('9 - Quit')
+    choice = input('make a choice: ')
+    print(choice)
+    return choice
+interface()
---
+++ returns.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    c = a + b
+    return c
+answer = add(3, 5)
+print(answer)
+
