1fa633ad6b2efc57d87d7f939883efb403d35087
Zhouyang Min <zm66@duke.edu>
2021-09-01 12:18:30 -0400

    add database.py file

--- blood_calculation.py
+++ blood_calculation.py
@@ -6,3 +6,11 @@ def interface():
     print(choice)
     return choice
 interface()
+
+
+def HDL_Driver():
+    HDL_value = hdl_input()
+    HDL_character = hdl_analysis(HDL_value)
+    hdl_output(HDL_vlue, HDL_character)
+
+def hdl_input()
---
+++ database.py
@@ -0,0 +1,5 @@
+import blood_calculator
+
+answer = blood_calculator.hdl_analysis(55)
+print('The analysis of 55 HDL is {}'.format(answer))
+answer2 = blood_calculator.ldl_analysis(200)
