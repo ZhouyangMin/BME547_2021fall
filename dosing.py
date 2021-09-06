1ec9344984878db6a368e304a59d4c3fd9551b7d
Zhouyang Min <zm66@duke.edu>
2021-09-06 12:47:15 -0400

    Modularized soing but still has some syntax problems

--- dosing.py
+++ dosing.py
@@ -18,21 +18,42 @@ def dose_amount():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
-    weight_input = float(input("Enter weight: "))
+    weight_input = (input("Enter weight: "))
+    return diagnosis, weight_input
+
+
+
+
+def unit_converstion(input_lb):
+    weight = weight / 2.205
+    return weight
+
+
+def calculations(diagnosis, weight):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
-        weight = weight / 2.205
+        weight = unit_converstion(weight)
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kgdoi = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return weight, dosage_mg_first_day
+
+
+def data_ouput(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
 
+def program_driver():
+    diag, w_input = info_input()
+    wt, dose_1 = calculations(diag, w_input)
+    data_output(wt, dose_1)
+
 if __name__ == '__main__':
-    dose_amount()
+    program_driver()
+
 
