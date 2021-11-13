ea0e86010baa51922c073e13faff3c01a942d562
Zhouyang Min <zm66@duke.edu>
2021-11-12 23:42:58 -0500

    create the test function

--- test_twopointform.py
+++ test_twopointform.py
@@ -1,11 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("inputValue, expected", [
-  
-    ])
-
-def test_twopointform(inputValue, expected):
+@pytest.mark.parametrize("x1, y1, x2, y2, input_x, expected", [
+    (1, 2, 3, 4, 5, 6), (2, 2, 4, 16, 7, 37), (3, 9, 7, 93, 1.6, -20.4)
+])
+def test_twopointform(x1, y1, x2, y2, input_x, expected):
     from twopointform import twopointform
-    answer = twopointform(inputValue)
+    answer = twopointform(x1, y1, x2, y2, input_x)
     assert answer == expected
